d86b5254fa9106e389a93cd8d6286f186890b398
Zach Cullen <zacullen@gmail.com>
2020-09-14 13:15:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+  let newMin = array[0]
+  let minIndex = 0
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < newMin) {
+      newMin = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1) // remove element in place at index where min was
+  return newMin
 }
 
 function insertionSort(array){
-
-}
+  let unsorted = array
+  let sorted = []
+  while (unsorted.length > 0) {
+    sorted.push(findMinAndRemove(array))
+  }
+  return sorted
+} 
