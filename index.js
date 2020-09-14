eb124e47e30c79f97fa4b015f410b33dfb0721c1
Zach Cullen <zacullen@gmail.com>
2020-09-14 13:11:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function findMinAndRemove(array){
-  
+  let newMin = array[0]
+  let minIndex = 0
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < newMin) {
+      newMin = array[i]
+    }
+  }
+  array.splice(minIndex, 1)
+  return newMin
 }
 
 function insertionSort(array){
